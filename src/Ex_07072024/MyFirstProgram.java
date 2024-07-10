57040a4d86411e3e78fa5b273a572be1ae1fa752
Srajan Falke <srajan.falke@gmail.com>
2024-07-10 20:27:54 +0530

    this is the new changes

--- src/Ex_07072024/MyFirstProgram.java
+++ src/Ex_07072024/MyFirstProgram.java
@@ -2,6 +2,6 @@ package Ex_07072024;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("this is my first program");
+        System.out.println(2/2);
     }
 }
